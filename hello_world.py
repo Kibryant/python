c4105a602b5044a1386b43c9d19d6c4a3ae66bab
Kibryant <arthurgustavon@gmail.com>
2024-01-03 15:58:10 -0300

    first commit

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+def maximum_count(mums: list[int]) -> int:
+    return max(
+        len([mum for mum in mums if mum < 0]),
+        len([mum for mum in mums if mum > 0]),
+    )
+
+result = maximum_count([-1, -2, -3, -4, -5, -6, 7, 8, 9, 10]) 
+
+print(result)
